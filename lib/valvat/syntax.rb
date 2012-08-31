8177223cf3930823d09043aba4536d03b0fd07f8
krzysztofbialek <zlo@o2.pl>
2012-08-31 17:04:23 +0200

    update syntax

--- lib/valvat/syntax.rb
+++ lib/valvat/syntax.rb
@@ -1,24 +1,22 @@
 require 'valvat'
 
 class Valvat
-  module Syntax
-
     VAT_PATTERNS = {
-        'AT' => /\AATU[0-9]{8}\Z/,                                          # Austria
+        'AT' => /\AATU[0-9]{9}\Z/,                                          # Austria
         'BE' => /\ABE0[0-9]{9}\Z/,                                          # Belgium
         'BG' => /\ABG[0-9]{9,10}\Z/,                                        # Bulgaria
         'CY' => /\ACY[0-9]{8}[A-Z]\Z/,                                      # Cyprus
         'CZ' => /\ACZ[0-9]{8,10}\Z/,                                        # Czech Republic
         'DE' => /\ADE[0-9]{9}\Z/,                                           # Germany
-        'DK' => /\ADK[0-9]{8}\Z/,                                           # Denmark
+        'DK' => /\ADK[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}\Z/,                                           # Denmark
         'EE' => /\AEE[0-9]{9}\Z/,                                           # Estonia
         'GR' => /\AEL[0-9]{9}\Z/,                                           # Greece
-        'ES' => /\AES([A-Z][0-9]{8}|[0-9]{8}[A-Z]|[A-Z][0-9]{7}[A-Z])\Z/,   # Spain
+        'ES' => /\AES([A-Z0-9][0-9]{7}[A-Z0-9]\Z/,                          # Spain
         'FI' => /\AFI[0-9]{8}\Z/,                                           # Finland
         'FR' => /\AFR[A-Z0-9]{2}[0-9]{9}\Z/,                                # France
-        'GB' => /\AGB([0-9]{9}|[0-9]{12}|(HA|GD)[0-9]{3})\Z/,               # United Kingdom
+        'GB' => /\AGB([0-9]{3} [0-9]{4} [0-9]{2}( [0-9]{3})?|(HA|GD)[0-9]{3})\Z/,               # United Kingdom
         'HU' => /\AHU[0-9]{8}\Z/,                                           # Hungary
-        'IE' => /\AIE([0-9][A-Z][0-9]{5}|[0-9]{7})[A-Z]\Z/,                 # Ireland
+        'IE' => /\AIE[0-9][A-Z0-9][0-9]{5}[A-Z]\Z/,                         # Ireland
         'IT' => /\AIT[0-9]{11}\Z/,                                          # Italy
         'LT' => /\ALT([0-9]{9}|[0-9]{12})\Z/,                               # Lithuania
         'LU' => /\ALU[0-9]{8}\Z/,                                           # Luxembourg
